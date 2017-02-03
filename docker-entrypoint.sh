164dd1c8ffea7aaedef09c70a1e9497de1097284
Sergey <zotovsa@gmail.com>
2017-02-03 20:23:28 +0300

    fixed shell issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
 #ip=$(grep `hostname` /etc/hosts | awk '{print $1}')
-#exec /opt/jboss/datagrid/bin/standalone.sh -c clustered.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
+#exec /opt/jboss/keycloak/bin/standalone.sh -c clustered.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
 
-exec /opt/jboss/datagrid/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
+exec /opt/jboss/keycloak/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
 
 
 #exec /opt/jboss/keycloak/bin/standalone.sh $@
