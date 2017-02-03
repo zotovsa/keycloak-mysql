731524180032f6792b39580d7bb1c635d5ad940d
Sergey <zotovsa@gmail.com>
2017-02-03 19:54:02 +0300

    changed to single instance

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 Run mysql:
 
 
-c new-app mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=keycloak -e MYSQL_USER=keycloak -e MYSQL_PASSWORD=keycloak
+oc new-app mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=keycloak -e MYSQL_USER=keycloak -e MYSQL_PASSWORD=keycloak
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ fi
 #ip=$(grep `hostname` /etc/hosts | awk '{print $1}')
 #exec /opt/jboss/datagrid/bin/standalone.sh -c clustered.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
 
-exec /opt/jboss/keycloak/bin/standalone.sh $@
+exec /opt/jboss/datagrid/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$ip -Djboss.bind.address.management=0.0.0.0 -Djgroups.join_timeout=1000 -Djboss.default.jgroups.stack=kubernetes
+
+
+#exec /opt/jboss/keycloak/bin/standalone.sh $@
 exit $?
